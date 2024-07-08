4a3e2024059e6961e89c6aa047ca063d9c9fd472
speedfreak <gdramze@gmail.com>
2024-07-08 16:44:53 +0300

    add materials

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название курса')
+    img = models.ImageField(upload_to='courses/', verbose_name='Картинка')
+    description = models.TextField(verbose_name='Описание курса')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название урока')
+    img = models.ImageField(upload_to='lessons/', verbose_name='Картинка')
+    description = models.TextField(verbose_name='Описание урока')
+    link = models.CharField(max_length=200, verbose_name='Ссылка')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Курс')
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,7 @@
+from users.apps import UsersConfig
 
+app_name = UsersConfig.name
+
+urls = [
+
+]
