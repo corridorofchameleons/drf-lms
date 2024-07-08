a7bf31f7f81ab354edcdf2fda58c5a1a28ab6da2
speedfreak <gdramze@gmail.com>
2024-07-08 18:14:32 +0300

    add User RetrieveUpdate

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+from users.views import UserRetrieveUpdateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path('<int:pk>/', UserRetrieveUpdateAPIView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,1 +1,9 @@
+from rest_framework import generics
 
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
