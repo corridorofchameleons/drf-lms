9caf94788575925d26ec9c7e69c4116fe849ce46
speedfreak <gdramze@gmail.com>
2024-07-08 18:07:15 +0300

    add lesson CRUD

--- materials/urls.py
+++ materials/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from materials.apps import MaterialsConfig
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonListAPIView, LessonUpdateAPIView, LessonDestroyAPIView, \
+    LessonRetrieveAPIView, LessonCreateAPIView
 
 app_name = MaterialsConfig.name
 
@@ -11,4 +12,10 @@ router.register('', CourseViewSet)
 
 urlpatterns = [
     path('courses/', include(router.urls)),
+
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_details'),
+    path('lessons/', LessonListAPIView.as_view(), name='lesson_list'),
 ]
