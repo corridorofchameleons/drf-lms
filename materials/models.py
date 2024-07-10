b566620e76876c3c0e5538cc9a4954f2dcb47d93
speedfreak <gdramze@gmail.com>
2024-07-10 16:41:36 +0300

    add Payment

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 
 
@@ -5,6 +6,8 @@ class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название курса')
     img = models.ImageField(upload_to='courses/', verbose_name='Картинка', null=True, blank=True)
     description = models.TextField(verbose_name='Описание курса')
+
+    payments = GenericRelation('users.Payment')
 
     class Meta:
         verbose_name = 'Курс'
@@ -17,6 +20,8 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='Описание урока')
     link = models.CharField(max_length=200, verbose_name='Ссылка', null=True, blank=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name='Курс')
+
+    payments = GenericRelation('users.Payment')
 
     class Meta:
         verbose_name = 'Урок'
--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -16,3 +18,20 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    payment_date = models.DateField(auto_now_add=True, verbose_name='Дата оплаты')
+
+    # создадим полиморфную связь
+    content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey(ct_field='content_type', fk_field='object_id')
+
+    def __str__(self):
+        return f'{self.user}, {self.payment_date} - {self.content_object}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
